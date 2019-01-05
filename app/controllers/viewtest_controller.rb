498cccd646d830ed9d225cebf78617f984d0ccf3
ato-s <a.saidera@4peace.co.jp>
2019-01-05 11:33:08 +0900

    コントローラーの設定

--- app/controllers/viewtest_controller.rb
+++ app/controllers/viewtest_controller.rb
@@ -1,7 +1,8 @@
 class ViewtestController < ApplicationController
-  layout "single", only: [:login, :signup]
+  layout "single", only: [:login, :signup, :sell]
   def index; end
   def login; end
   def signup; end
   def signup_registration; end
+  def sell; end
 end
