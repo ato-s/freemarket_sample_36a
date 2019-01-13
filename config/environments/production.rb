aef3daaeaf50b66b1f0473f74787a0711fc7af0e
takuya-teramoto <t.teramon23@gmail.com>
2019-01-13 16:14:21 +0900

    create setting for mailer in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,12 +60,18 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "freemarket_sample_36a_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'http://アプリ名' }
   config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "freemarket.sample.36a@gmail.com",
+    :password => Rails.application.secrets.gmail_password,
+    :authentication => :plain
+}
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
