a57777c8f364f8ace9785ee2984221043852e2cc
takuya-teramoto <t.teramon23@gmail.com>
2019-01-19 16:36:03 +0900

    change mailer domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "freemarket_sample_36a_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'http://アプリ名' }
+  config.action_mailer.default_url_options = { host: 'http://tech-expert.freemarket-sample-36a.com/' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
