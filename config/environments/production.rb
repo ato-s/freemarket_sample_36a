3807a1b565ed35da53d82d7b56d4643408f6d805
takuya-teramoto <t.teramon23@gmail.com>
2019-01-20 20:12:20 +0900

    deploy test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
 
   config.assets.compile = false
 
