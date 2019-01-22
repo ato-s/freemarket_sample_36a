d6a79af8c85ed484404ed57183e133fa8b91b537
takuya-teramoto <t.teramon23@gmail.com>
2019-01-22 21:07:44 +0900

    user testcode (pre)

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+
+  factory :devise_user do
+    nickname              "abe"
+    email                 "kkk@gmail.com"
+    password              "00000000"
+    password_confirmation "00000000"
+  end
+
+  factory :api_user do
+    nickname              "abe"
+    email                 "kkk@gmail.com"
+    password              "00000000"
+    password_confirmation "00000000"
+  end
+
+end
