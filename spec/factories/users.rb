19e5053c9b6b292b35665ae254e7b73eceadc741
takuya-teramoto <t.teramon23@gmail.com>
2019-01-23 21:48:31 +0900

    definition of devise_user and api_user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,29 @@
 FactoryBot.define do
 
-  factory :devise_user do
-    nickname              "abe"
-    email                 "kkk@gmail.com"
+  factory :devise_user, class: User do
+    email                 "test_user@gmail.com"
+    nickname              "test_user"
+    uid                   ""
+    provider              ""
+    good_count            "0"
+    normal_count          "0"
+    bad_count             "0"
+    created_at            ""
+    updated_at            ""
     password              "00000000"
     password_confirmation "00000000"
   end
 
-  factory :api_user do
-    nickname              "abe"
-    email                 "kkk@gmail.com"
+  factory :api_user, class: User do
+    email                 ""
+    nickname              ""
+    uid                   "uid"
+    provider              "instagram"
+    good_count            "0"
+    normal_count          "0"
+    bad_count             "0"
+    created_at            ""
+    updated_at            ""
     password              "00000000"
     password_confirmation "00000000"
   end
