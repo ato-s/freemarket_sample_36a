2995fe8a4978ae049e9561373aa02e58ecd388b3
ato-s <a.saidera@4peace.co.jp>
2019-01-26 12:00:51 +0900

    パンくずリストの設定ファイルを作成

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,23 @@
+crumb :root do
+  link 'メルカリ', root_path
+end
+
+crumb :categories do
+  link 'カテゴリー一覧', upper_categories_path
+  parent :root
+end
+
+crumb :upper_categories do |upper_category|
+  link upper_category.name, upper_category_path(upper_category)
+  parent :categories, upper_category
+end
+
+crumb :middle_categories do |middle_category|
+  link middle_category.name, middle_category_path(middle_category)
+  parent :upper_categories, middle_category.upper_category
+end
+
+crumb :lower_categories do |lower_category|
+  link lower_category.name, lower_category_path(lower_category)
+  parent :middle_categories, lower_category.middle_category
+end
