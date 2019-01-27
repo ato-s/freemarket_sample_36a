d5fa20bf23a2de2db96b378f194fa614812c309b
takuya-teramoto <t.teramon23@gmail.com>
2019-01-27 17:11:06 +0900

    set devise layout in config/application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_devise_layout, if: :devise_controller?
   before_action :set_categories
 
   protected
@@ -12,16 +11,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname, :uid, :provider, :good_count, :normal_count, :bad_count])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :uid, :provider, :good_count, :normal_count, :bad_count])
   end
-
-  def set_devise_layout
-    "layout_name_for_devise"
-  end
-
-
   def after_sign_out_path_for(resource)
     root_path
   end
-
   def after_sign_in_path_for(resource)
     if current_user.addresses.present?
       root_path
