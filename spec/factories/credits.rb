31b936f8b7b03e97125ac0a36f65233953b94947
金丸亮二 <ryouji0623.k@gmail.com>
2019-02-01 22:09:16 +0900

    credit_controller単体テスト用のfactory_botファイル、インデントを調整した

--- spec/factories/credits.rb
+++ spec/factories/credits.rb
@@ -16,14 +16,14 @@ FactoryBot.define do
   end
 
   factory :credit do
-    credit_number   {"4242424242424242"}
-    limit_month     {"12"}
-    limit_year      {"29"}
-    security_code   {"123"}
-    customer_id     {"cus_b8ee086801a5b26badf7e06dc935"}
-    user_id         {"1"}
-    created_at            {"2019-1-12 10:00:00"}
-    updated_at            {"2019-1-12 10:00:00"}
+    credit_number  {"4242424242424242"}
+    limit_month    {"12"}
+    limit_year     {"29"}
+    security_code  {"123"}
+    customer_id    {"cus_b8ee086801a5b26badf7e06dc935"}
+    user_id        {"1"}
+    created_at     {"2019-1-12 10:00:00"}
+    updated_at     {"2019-1-12 10:00:00"}
   end
 
 end
