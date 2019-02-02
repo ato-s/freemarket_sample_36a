b6941a951055017ba1810223b97f6b9d92646eff
takuya-teramoto <t.teramon23@gmail.com>
2019-02-02 19:28:31 +0900

    Update buys_controller.rb

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,29 +1,21 @@
 class BuysController < ApplicationController
+  before_action :set_item
+  before_action :confirm_item_seller_and_buyer
+  before_action :confirm_transaction_stage_under_sale
 
-before_action :set_item
-
-include Payjp_process
+  include Payjp_process
 
   def edit
-    if @item.transaction_stage == "sale"
-      @image = Picture.find(params[:item_id])
-      @address = Address.find_by(user_id: current_user.id)
-      @credit_data = show_customer_data
-    else
-      redirect_to root_path, alert: "既に販売済みの商品です"
-    end
+    @image = Picture.find(params[:item_id])
+    @address = Address.find_by(user_id: current_user.id)
+    @credit_data = show_customer_data
   end
 
   def update
-    if @item.transaction_stage == "sale"
-      @buy_price = @item.sell_price
-      @buyer_id = current_user.id
-      @transaction = @item.update(buy_price: @buy_price, buyer_id: @buyer_id,transaction_stage: 1)
-      create_charge(@buy_price)
-      redirect_to edit_item_buy_path
-    else
-      redirect_to root_path, alert: "既に販売済みの商品です"
-    end
+    @item.update(buy_params)
+    @payment = @item.buy_price - @item.discount_point
+    create_charge(@payment)
+    redirect_to edit_item_buy_path
   end
 
   private
@@ -31,5 +23,13 @@ include Payjp_process
   def set_item
     @item = Item.find(params[:item_id])
   end
-
+  def buy_params
+    params.require(:patch).permit(:discount_point, :shipping_address_id).merge(buyer_id: current_user.id, transaction_stage: "under_transaction")
+  end
+  def confirm_item_seller_and_buyer
+    redirect_to root_path, alert: "出品した商品を購入することはできません" if @item.seller == current_user
+  end
+  def confirm_transaction_stage_under_sale
+    redirect_to root_path, alert: "既に販売済みの商品です" unless @item.transaction_stage == "under_sale"
+  end
 end
