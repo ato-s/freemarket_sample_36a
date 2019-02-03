231b901a82f311828bb57c2bc9e74569783f211a
takuya-teramoto <t.teramon23@gmail.com>
2019-02-03 18:13:34 +0900

    create review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :set_item, only: [:new, :edit, :create, :update]
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :confirm_transaction_stage_under_transaction, except: [:index, :show]
+  before_action :confirm_item_appraiser_existence_and_item_buyer_already_create_review, only: [:new, :create]
+  after_action :update_transaction_stage_to_sold_out, only: :create
 
   def index
     @recieved_reviews = current_user.received_reviews
@@ -19,7 +22,7 @@ class ReviewsController < ApplicationController
 
   def create
     if @review = Review.create(review_params)
-      redirect_to @review, notice: 'Review was successfully created.'
+      redirect_to mypages_path, notice: 'Review was successfully created.'
     else
       render :new
     end
@@ -27,7 +30,7 @@ class ReviewsController < ApplicationController
 
   def update
     if @review = Review.update(review_params)
-      redirect_to @review, notice: 'Review was successfully created.'
+      redirect_to mypages_path, notice: 'Review was successfully created.'
     else
       render :edit
     end
@@ -49,5 +52,25 @@ class ReviewsController < ApplicationController
         @appraisee_id = @item.seller_id
       end
       params.require(:review).permit(:text, :evaluation, :item_id).merge(appraiser_id: current_user.id, appraisee_id: @appraisee_id, item_id: params[:item_id])
+    end
+    def confirm_transaction_stage_under_transaction
+      redirect_to mypages_path unless @item.transaction_stage == 'under_transaction'
+    end
+    def confirm_item_appraiser_existence_and_item_buyer_already_create_review
+      @item_appraiser_ids = ['dammy']
+      @item.reviews.each do |review|
+        @item_appraiser_ids << review.appraiser_id
+      end
+      redirect_to mypages_path if @item_appraiser_ids.include?(current_user.id)
+      if current_user == @item.seller
+        redirect_to mypages_path unless @item_appraiser_ids.include?(@item.buyer_id)
+      end
+    end
+    def update_transaction_stage_to_sold_out
+      if @item.reviews.length == 0
+        #create todo
+      elsif @item.reviews.length == 1
+        @item.update(transaction_stage: 'sold_out')
+      end
     end
 end
