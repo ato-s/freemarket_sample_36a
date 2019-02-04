13f86fe77b0d6fc704200e51d180a9be69c1e7b4
ato-s <a.saidera@4peace.co.jp>
2019-02-04 20:46:41 +0900

    modify deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/fm03b.pem']
+                  keys: ['~/.ssh/fm03.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
