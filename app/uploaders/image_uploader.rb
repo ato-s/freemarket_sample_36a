43e78846242296b925338be307194b9a94ce09fa
ato-s <a.saidera@4peace.co.jp>
2019-02-07 03:54:47 +0900

    fix s3 settings

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+  # storage :fog
 
 
   # # AWS
