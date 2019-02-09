8702c7eba4c55eaf6bc167e10cfb252332badf8d
takuya-teramoto <t.teramon23@gmail.com>
2019-02-09 14:38:44 +0900

    separate before action in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,8 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :confirm_buyer_or_seller_include_current_user, except: [:index, :show]
   before_action :confirm_transaction_stage_under_transaction, except: [:index, :show]
-  before_action :confirm_item_appraiser_existence_and_item_buyer_already_create_review, only: [:new, :create]
+  before_action :confirm_item_appraiser_exist, only: [:new, :create]
+  before_action :confirm_buyer_already_create_review, only: [:new, :create]
   after_action :update_transaction_stage_to_sold_out, only: :create
 
   def index
@@ -56,16 +57,19 @@ class ReviewsController < ApplicationController
       end
       params.require(:review).permit(:text, :evaluation, :item_id).merge(appraiser_id: current_user.id, appraisee_id: @appraisee_id, item_id: params[:item_id])
     end
-    def confirm_item_appraiser_existence_and_item_buyer_already_create_review
+    def confirm_item_appraiser_exist
       @item_appraiser_ids = ['dammy']
       @item.reviews.each do |review|
         @item_appraiser_ids << review.appraiser_id
       end
       redirect_to mypages_path if @item_appraiser_ids.include?(current_user.id)
+    end
+    def confirm_buyer_already_create_review
       if current_user == @item.seller
         redirect_to mypages_path unless @item_appraiser_ids.include?(@item.buyer_id)
       end
     end
+
     def update_transaction_stage_to_sold_out
       if @item.reviews.length == 0
         #create todo
