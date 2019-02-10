8498205463806b0c897dbd88cd182ec823619405
takuya-teramoto <t.teramon23@gmail.com>
2019-02-10 21:53:39 +0900

    add create_information in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
+  before_action :set_item
+  after_action :create_information, only: :create, unless: :information_is_exist?
 
   def create
     @like = Like.create(user_id: current_user.id, item_id: params[:item_id])
     @likes = Like.where(item_id: params[:item_id])
-    @item = Item.find(params[:item_id])
+    @information_type = 'be_liked'
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id]).destroy
-    @item = Item.find(params[:item_id])
   end
-
 end
