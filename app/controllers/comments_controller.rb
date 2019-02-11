2b3eac90d808e2244c9df2dc1ab5224de50e4684
takuya-teramoto <t.teramon23@gmail.com>
2019-02-11 18:41:44 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,17 @@
 class CommentsController < ApplicationController
   before_action :move_to_sign_in
-  before_action :set_item_and_comments
-
-  def index
-  end
+  before_action :set_item
 
   def create
     @created_comment = Comment.create(comment_params)
+    @information_type = 'be_commented'
+    if information_is_needed? && @item.seller_id != current_user.id
+      create_information
+    end
     redirect_to item_path(@item.id) ,notice: 'メッセージを投稿しました'
+  end
+
+  def destroy
   end
 
     private
@@ -15,11 +19,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-
-  def set_item_and_comments
-    @comment =Comment.new
-    @item = Item.find(params[:item_id])
-    @comments = @item.comments
-  end
-
 end
