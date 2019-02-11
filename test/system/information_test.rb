30ebb16bd0a196caeaf25b024ca5d68465089f60
takuya-teramoto <t.teramon23@gmail.com>
2019-02-11 11:35:33 +0900

    delete test files which isn't used

--- test/system/information_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class InformationTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit information_url
-  #
-  #   assert_selector "h1", text: "Information"
-  # end
-end
--- test/system/lower_categories_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class LowerCategoriesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit lower_categories_url
-  #
-  #   assert_selector "h1", text: "LowerCategory"
-  # end
-end
