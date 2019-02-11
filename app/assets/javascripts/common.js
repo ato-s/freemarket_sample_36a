ef9467e1c3a3e810f59361360c223362f3b25477
ato-s <a.saidera@4peace.co.jp>
2019-02-11 17:08:59 +0900

    スムーススクロールjsを追記

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -50,3 +50,14 @@ $(function() {
     }
   });
 });
+$(document).on('turbolinks:load', function() {
+  // common：smooth scroll
+  $('.js-scroll').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+})
