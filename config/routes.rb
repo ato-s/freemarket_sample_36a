e8e1d948e2014e4c0067759561020d395d8dae2f
takuya-teramoto <t.teramon23@gmail.com>
2019-02-12 23:02:58 +0900

    change routes for index of information, todo, buy items in mypage

--- config/routes.rb
+++ config/routes.rb
@@ -33,14 +33,16 @@ Rails.application.routes.draw do
   resources :lower_categories, only: [:show]
 
   get 'logout' => 'mypages#logout'
+  get 'index_todos' => 'mypages#index_todos'
+  get 'index_information' => 'mypages#index_information'
+  get 'index_under_transaction_items' => 'mypages#index_under_transaction_items'
+  get 'index_sold_out_items' => 'mypages#index_sold_out_items'
   resources :mypages, only: [:index, :show, :edit, :update]
   resources :likes, only: [:index]
   resources :reviews, only: [:index, :show]
   resources :addresses, only: [:index, :new, :create, :edit, :update]
   resources :phone_numbers, only: [:new, :create, :edit, :update]
   resources :credits, only: [:new, :create, :destroy, :index]
-  resources :information, only: [:index]
-  resources :todos, only: [:index]
 
   resource :safe, only: [:show] do
     resource :description,only: [:show]
