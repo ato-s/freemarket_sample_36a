543eaf20ad47906ca9803a6b8b9b56f5d9895cb2
takuya-teramoto <t.teramon23@gmail.com>
2019-02-14 21:50:11 +0900

    resolve brand initial problem

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,12 +4,26 @@ class GroupsController < ApplicationController
 
   def show
     @groups = Group.eager_load(:brands)
-    @initials = @group.brands.group(:initial_word)
     @brands = @group.brands
+    @ununique_initials = @brands.pluck(:initial_word)
+    @initials = @ununique_initials.uniq
+    find_border_of_different_initial
   end
 
   private
+
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  def find_border_of_different_initial
+    @initial_index =[]
+    @previous_initial = nil
+    @ununique_initials.each_with_index do |ununique_initial, index|
+      if @previous_initial != ununique_initial
+        @initial_index << index
+        @previous_initial = ununique_initial
+      end
+    end
   end
 end
