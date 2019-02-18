cbb003d91c80f12f9af05f402b00da7d4928207b
ato-s <a.saidera@4peace.co.jp>
2019-02-18 22:31:36 +0900

    no message

--- db/schema.rb
+++ db/schema.rb
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 20190214144942) do
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "report_type", default: 0, null: false
     t.index ["item_id"], name: "index_reports_on_item_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
