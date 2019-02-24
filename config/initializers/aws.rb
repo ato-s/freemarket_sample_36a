b68683b7ab8f36b70ab673efdc8cdd4a4d896868
西田昂平 <zelbondtech@gmail.com>
2019-02-24 21:16:05 +0900

    change aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,1 +1,6 @@
 require 'aws-sdk'
+
+Aws.config.update({
+  region: 'ap-northeast-1',
+  credentials: Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_secret_access_key)
+})
